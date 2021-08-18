3c9c86a0a992f2715fbd98db83da63a51214d37a
Vova-Nik <vmaseich@gmail.com>
2021-08-18 09:52:38 +0300

    fonetic corrections

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("I am your awesome Java application!");
-
+        System.out.println("I`ll ansver You");
         DeepThought ourSuperComputer = new DeepThought();
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
+
         System.out.println(ans);
     }
 }
